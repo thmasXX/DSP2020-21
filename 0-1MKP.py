aba40619943d71134e87a7bef519d9a54152cee0
Tom <tjsouth@hotmail.co.uk>
2021-01-08 13:50:19 +0000

    Add files via upload

--- 0-1MKP.py
+++ 0-1MKP.py
@@ -1,50 +1,50 @@
 import random
         
-class Particle:
-
-    def __init__(self, pos, velocity, fit):
-        self.pos = pos
-        self.velocity = velocity
-        self.fit = fit
-        self.pBest = [0] * len(pos)
-
-    def sigmoid(self, value):
-        return 1.0/(1.0 + math.exp((-1.0 * value))
-
-    def updatePos(self):
-        for i in range(0, len(self.position)):
-            random1 = random.random()
-            if (random1 < self.sigmoid(self.velocity[i])):
-                self.pos[i] = 1
-            else:
-                self.pos[i] = 0
-
-    def updateVelocity(self, gBest):
-        r1 = random.random()
-        r2 = random.random()
-        soc = c1 * r1 * (gBest - self.pos[i])
-        cog = c2 * r2 * (self.pBest - self.pos[i])
-        self.velocity[i] = (w * self_velocity[i]) + soc + cog
-
-    def updateBestParticle(self, p):
-        self.pBest = p
-
-    def updateFitness(self, f):
-        self.fit = f
-
-    def getPosition(self):
-        reutn self.pos[:]
-
-    def getPBest(self):
-        return self.pBest[:]
-
-    def getVelocity(self):
-        return self.velocity[:]
-
-    def getParticle(self):
-        strPos = (', '.join(str(x) for x in self.pos)
-        strVelocity = (', '.join(str(x) for x in self.velocity)
-        return ("position: " + strPos + " velocity: " + strVelocity + " fitness: " + str(self.fit))
+##class Particle:
+##
+##    def __init__(self, pos, velocity, fit):
+##        self.pos = pos
+##        self.velocity = velocity
+##        self.fit = fit
+##        self.pBest = [0] * len(pos)
+##
+##    def sigmoid(self, value):
+##        return 1.0/(1.0 + math.exp((-1.0 * value)))
+##
+##    def updatePos(self):
+##        for i in range(0, len(self.position)):
+##            random1 = random.random()
+##            if (random1 < self.sigmoid(self.velocity[i])):
+##                self.pos[i] = 1
+##            else:
+##                self.pos[i] = 0
+##
+##    def updateVelocity(self, gBest):
+##        r1 = random.random()
+##        r2 = random.random()
+##        soc = c1 * r1 * (gBest - self.pos[i])
+##        cog = c2 * r2 * (self.pBest - self.pos[i])
+##        self.velocity[i] = (w * self_velocity[i]) + soc + cog
+##
+##    def updateBestParticle(self, p):
+##        self.pBest = p
+##
+##    def updateFitness(self, f):
+##        self.fit = f
+##
+##    def getPosition(self):
+##        return self.pos[:]
+##
+##    def getPBest(self):
+##        return self.pBest[:]
+##
+##    def getVelocity(self):
+##        return self.velocity[:]
+##
+##    def getParticle(self):
+##        strPos = (', '.join(str(x) for x in self.pos))
+##        strVelocity = (', '.join(str(x) for x in self.velocity))
+##        return ("position: " + strPos + " velocity: " + strVelocity + " fitness: " + str(self.fit))
     
 def pso():
     knapsacks = []
